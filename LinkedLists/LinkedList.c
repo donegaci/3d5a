9fb03ebef8c682081f825afd873acfdff70fecc4
Ciaran Donegan <donegaci@lg12-15.scss.tcd.ie>
2018-09-28 14:54:31 +0100

    Working on deleteList fucntion

--- LinkedLists/LinkedList.c
+++ LinkedLists/LinkedList.c
@@ -11,10 +11,12 @@ struct Node {
 
 void addNode(Node** list, char data);
 void printList(Node* list);
+void removeNode(Node **list, char data);
+void deleteList(Node **list);
 
-Node *head = NULL;
 
 int main(){
+    Node *head = NULL;
 
     addNode(&head, 'A');
     addNode(&head, 'B');
@@ -22,31 +24,79 @@ int main(){
     addNode(&head, 'D');
     printList(head);
 
-    return 0;
+    // removeNode(&head, 'D');
+    // printList(head);
+    // removeNode(&head, 'B');
+    // printList(head);
+    // removeNode(&head, 'A');
+    // printList(head);
 
+    deleteList(&head);
+    printList(head);
+
+    return 0;
 }
 
 
 void addNode(Node** list, char data){
-    if (head == NULL){ // the list is empty
-        head = (Node*)malloc( sizeof( Node ) );
-        head->data = data;
-        head->next = NULL;
+
+    Node *new = (Node*)malloc( sizeof( Node ) );
+    new->data = data;
+
+    if (*list == NULL){ // the list is empty
+        new->next = NULL;
+        *list = new;
     }
     else{
-        Node *oldHead = head;
-        head = (Node*)malloc( sizeof( Node ) );
-        head->data = data;
-        head->next = oldHead;
-
+        new->next = *list;
+        *list = new;
     }
 }
 
+
 void printList(Node* list){
-    Node *current = head;
+    Node *current = list;
 
     while( current != NULL){
         printf("%c\n", current->data);
         current = current->next;
+    }
+}
+
+
+void removeNode(Node **list, char data){
+    Node *current = *list;
+    Node *prev = NULL;
+    int found = 0;
+
+    while( current != NULL && !found){
+        if (current->data == data){
+            found = 1;
+            //printf("Found\n");
+
+            if (prev == NULL){ // the head has to be removed
+                free(current);
+            }
+            else{
+                prev->next = current-> next;
+                free(current);
+            }
+
+        }
+        prev = current;
+        current = current->next;
+    }
+}
+
+
+void deleteList(Node **list){
+    Node *current = *list;
+    Node *next = *list;
+
+    while (current != NULL){
+        next = current->next;
+        printf("About to delete %c\n", current->data);
+        free(current);
+        current = next;
     }
 }
