87d79e9773355ffe14f0bcef4313710bb8e33088
Ciaran Donegan <ciaran@Ciarans-MacBook-Pro.local>
2018-10-27 21:49:55 +0100

    Created testPerformance functon

--- Assingment2/Task1.c
+++ Assingment2/Task1.c
@@ -7,6 +7,7 @@ void quickSort(int arr[], int left, int right);
 int partition(int arr[], int left, int right);
 void swap(int arr[], int index1, int index2);
 void printArray(int arr[], int size);
+void testPerformance();
 
 static int numSwaps=0;
 static int numComparisons=0;
@@ -19,42 +20,12 @@ int main(){
     int test4[SIZE] = {5, 4, 3, 2, 1, 0};
     int test5[SIZE] = {3, 3, 3, 3, 3, 3};
     
-    quickSort(test0, 0, 9);
-    // printArray(test0, 10);
-    printf("10 random values \n#Swaps: %d\n", numSwaps);
-    printf("#Comparisons: %d\n\n", numComparisons);
+    // quickSort(test0, 0, 9);
+    // // printArray(test0, 10);
+    // printf("10 random values \n#Swaps: %d\n", numSwaps);
+    // printf("#Comparisons: %d\n\n", numComparisons);
 
-    {
-        numSwaps=0; 
-        numComparisons=0;
-        quickSort(test1, 0, 5);
-        printf("Unique Random Values \n#Swaps: %d\n", numSwaps);
-        printf("#Comparisons: %d\n\n", numComparisons);
-
-        numSwaps=0; 
-        numComparisons=0;
-        quickSort(test2, 0, 5);
-        printf("Random Values \n#Swaps: %d\n", numSwaps);
-        printf("#Comparisons: %d\n\n", numComparisons);
-
-        numSwaps=0; 
-        numComparisons=0;
-        quickSort(test3, 0, 5);
-        printf("Sorted Array \n#Swaps: %d\n", numSwaps);
-        printf("#Comparisons: %d\n\n", numComparisons);
-
-        numSwaps=0; 
-        numComparisons=0;
-        quickSort(test4, 0, 5);
-        printf("Reverse Sorted Array \n#Swaps: %d\n", numSwaps);
-        printf("#Comparisons: %d\n\n", numComparisons);
-
-        numSwaps=0; 
-        numComparisons=0;
-        quickSort(test4, 0, 5);
-        printf("Uniform list \n#Swaps: %d\n", numSwaps);
-        printf("#Comparisons: %d\n\n", numComparisons);
-    }
+    testPerformance();
 
     return 0;
 }
@@ -121,4 +92,54 @@ void quickSort(int arr[], int left, int right){
     /* Sort the two resulting partitions*/
     quickSort(arr, left, partIndex -1);
     quickSort(arr, partIndex+1, right);
+}
+
+void testPerformance(){
+    
+    int test1[SIZE] = {4, 3, 5, 1, 0, 2};
+    int test2[SIZE] = {3, 3, 2, 1, 1, 4};
+    int test3[SIZE] = {0, 1, 2, 3, 4, 5};
+    int test4[SIZE] = {5, 4, 3, 2, 1, 0};
+    int test5[SIZE] = {3, 3, 3, 3, 3, 3};
+
+    numSwaps=0; 
+    numComparisons=0;
+    printArray(test1, 5);
+    quickSort(test1, 0, 5);
+    printArray(test1, 5);
+    printf("Unique Random Values \n#Swaps: %d\n", numSwaps);
+    printf("#Comparisons: %d\n\n", numComparisons);
+
+    numSwaps=0; 
+    numComparisons=0;
+    printArray(test2, 5);
+    quickSort(test2, 0, 5);
+    printArray(test2, 5);
+    printf("Random Values \n#Swaps: %d\n", numSwaps);
+    printf("#Comparisons: %d\n\n", numComparisons);
+
+    numSwaps=0; 
+    numComparisons=0;
+    printArray(test3, 5);
+    quickSort(test3, 0, 5);
+    printArray(test3, 5);
+    printf("Sorted Array \n#Swaps: %d\n", numSwaps);
+    printf("#Comparisons: %d\n\n", numComparisons);
+
+    numSwaps=0; 
+    numComparisons=0;
+    printArray(test4, 5);
+    quickSort(test4, 0, 5);
+    printArray(test4, 5);
+    printf("Reverse Sorted Array \n#Swaps: %d\n", numSwaps);
+    printf("#Comparisons: %d\n\n", numComparisons);
+
+    numSwaps=0; 
+    numComparisons=0;
+    printArray(test5, 5);
+    quickSort(test5, 0, 5);
+    printArray(test5, 5);
+    printf("Uniform list \n#Swaps: %d\n", numSwaps);
+    printf("#Comparisons: %d\n\n", numComparisons);
+    
 }
