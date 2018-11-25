7a687ac0996cf0d58c4401031906d7c0015efc6a
Ciaran Donegan <ciaran@Ciarans-MacBook-Pro.local>
2018-11-25 20:41:14 +0000

    DFS and BFS working but not alphabetical order

--- Assingment4/Task1.c
+++ Assingment4/Task1.c
@@ -1,29 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h> // to use function tolower()
-
-typedef struct Node Node;
-struct Node{
-    int vertex; // I will represent ascii characters as integers
-    Node* next;
-};
-
-// typedef struct AdjList AdjList;
-// struct AdjList{
-//     Node* head;
-// };
-
-typedef struct Graph Graph;
-struct Graph{
-    int numVertices;
-    Node** array;
-};
-
-Node* createNode(char name);
-Graph* createGraph(int nVertices);
-void initialiseGraph(Graph* graph);
-void insertEdge(Graph* graph, char source, char dest );
-void printGraph(Graph* graph);
+#include "Task1.h"
 
 
 int main(){
@@ -35,11 +10,19 @@ int main(){
     
     printGraph(graph);
 
+    printf("DFS: ");
+    depthFirstSearch(graph,'A');
+    printf("\n");
+
+    printf("BFS: ");
+    breadthFirstSearch(graph,'A');
+    printf("\n");
+
     return 0;
 }
 
 
-Node* createNode(char name){
+Node* createNode(int name){
     Node* newNode= (Node*)malloc(sizeof(Node*));
     newNode->vertex = name;
     newNode->next = NULL;
@@ -51,9 +34,13 @@ Graph* createGraph(int nVertices){
     Graph* graph = malloc(sizeof(Graph));
     graph->numVertices = nVertices;
     graph->array = malloc(nVertices * sizeof(Node*));
+    graph->visited = malloc(nVertices * sizeof(int));
 
-    for(int i=0; i<nVertices; i++)
+    // I want to be able to access my array with the chars so
+    for(int i= 'A'; i < ('A' + nVertices); i++){
         graph->array[i] = NULL;
+        graph->visited[i] = 0; // set visited to false
+    }
 
     return graph;
 }
@@ -61,39 +48,128 @@ Graph* createGraph(int nVertices){
 
 void initialiseGraph(Graph* graph){
     insertEdge(graph, 'A', 'B');
-    insertEdge(graph, 'A', 'E');
     insertEdge(graph, 'A', 'D');
+    insertEdge(graph, 'A', 'E');
     insertEdge(graph, 'B', 'C');
-    insertEdge(graph, 'C', 'B');
     insertEdge(graph, 'B', 'D');
+    insertEdge(graph, 'C', 'B');
     insertEdge(graph, 'C', 'D');
     insertEdge(graph, 'D', 'F');
-    insertEdge(graph, 'F', 'C');
     insertEdge(graph, 'E', 'D');
+    insertEdge(graph, 'F', 'C');
 }
 
 
-void insertEdge(Graph* graph, char source, char dest ){
-    // Covert all chars to lower case and subtract the ascii value of 'a'
-    // so that a=0, b=1, c=2, ... etc.
-    int srcIndex = tolower(source) - 'a';
-
+void insertEdge(Graph* graph, int source, int dest ){
     Node* newNode = createNode(dest);
-    newNode->next = graph->array[srcIndex];
-    graph->array[srcIndex] = newNode;
-
+    newNode->next = graph->array[source];
+    graph->array[source] = newNode;
 }
+
 
 void printGraph(Graph* graph){
 
-    for(int i=0; i<graph->numVertices; i++){
+     for(int i= 'A'; i<('A' + graph->numVertices); i++){
         Node* temp = graph->array[i];
-        printf("%c : ", (i+'A'));
+        printf("%c : ", i);
         while(temp){
             printf(" -> %c", temp->vertex);
             temp = temp->next;
         }
         printf("\n");
     }
+}
 
+
+// Recursive function
+void depthFirstSearch(Graph* graph, int vertex) {
+
+    Node* temp = graph->array[vertex];
+    graph->visited[vertex] = 1;
+
+    printf("%c ", vertex);
+
+    while(temp){
+        int edge = temp->vertex;
+        if(graph->visited[edge] == 0) // if not visited
+            depthFirstSearch(graph, edge);
+        temp = temp->next;
+    }
+}
+
+
+Queue* createQueue(){
+    Queue* q = malloc(sizeof(Queue));
+    // initialise pointers to empty
+    q->front = -1;
+    q->rear = -1;
+    return q;
+}
+
+
+void enqueue(Queue* q, int value){
+    if(q->rear == MAX -1)
+        printf("Queue is full \n");
+    else{
+        if(q->front == -1) // if it was empty
+            q->front =0; // show that there is something in it
+        q->rear++;
+        q->items[q->rear] = value;
+    }
+}
+
+
+int dequeue(Queue* q){
+    if(isEmpty(q)){
+        printf("Queue is empty");
+        return -1;
+    }
+    else{
+        int value = q->items[q->front];
+        q->front++;
+        if(q->front > q->rear) // if there is nothing left
+            q->front = q->rear = -1;
+        return value;
+    }
+}
+
+
+int isEmpty(Queue* q) {
+    if(q->rear == -1) 
+        return 1;
+    else 
+        return 0;
+}
+
+
+void setNotVisited(Graph* graph){
+    for(int i= 'A'; i<('A' + graph->numVertices); i++)
+        graph->visited[i] = 0;
+}
+
+
+void breadthFirstSearch(Graph* graph, int vertex){
+    Queue* q = createQueue();
+
+    setNotVisited(graph);
+
+    graph->visited[vertex] = 1; // set current to visited
+    enqueue(q, vertex);
+
+    while(!isEmpty(q)){ // while not empty
+        int current = dequeue(q);
+        printf("%c ", current);
+
+        Node* temp = graph->array[current];
+
+        while(temp){
+            int nextVertex = temp->vertex;
+
+            if(graph->visited[nextVertex] == 0){ // if not already visited
+                graph->visited[nextVertex] = 1;
+                enqueue(q, nextVertex);
+            }
+            temp = temp->next;
+        }
+    }
 }
---
+++ Assingment4/Task1.h
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h> // to use function tolower()
+#define MAX 50 
+
+typedef struct Node Node;
+struct Node{
+    int vertex;
+    Node* next;
+};
+typedef struct Graph Graph;
+struct Graph{
+    int numVertices;
+    int* visited;
+    Node** array; // 2d array basically
+};
+typedef struct Queue Queue;
+struct Queue{
+    int items[MAX];
+    int front;
+    int rear;
+};
+
+Node* createNode(int name);
+Graph* createGraph(int nVertices);
+void initialiseGraph(Graph* graph);
+void insertEdge(Graph* graph, int source, int dest );
+void printGraph(Graph* graph);
+void depthFirstSearch(Graph* graph, int vertex);
+Queue* createQueue();
+void enqueue(Queue* q, int value);
+int dequeue(Queue* q);
+int isEmpty(Queue* q);
+void setNotVisited(Graph* graph);
+void breadthFirstSearch(Graph* graph, int vertex);
